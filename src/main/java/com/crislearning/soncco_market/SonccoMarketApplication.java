a7d785415a91d09517ad849e72c8c2056ee7412b
Cristian Soncco <cristian_soncco@msn.com>
2025-01-04 18:25:45 +0100

    Compromiso inicial: configurar la estructura del proyecto Spring Boot con Gradle, incluidos los archivos de configuración, las propiedades de la aplicación y la clase básica de la aplicación. Se agregaron .gitignore y .gitattributes para configuraciones específicas del entorno.

---
+++ src/main/java/com/crislearning/soncco_market/SonccoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.crislearning.soncco_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonccoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonccoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crislearning/soncco_market/SonccoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crislearning.soncco_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonccoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
