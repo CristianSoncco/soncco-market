a8de1090f42f776d879042031a82c4014b3011dc
Cristian Soncco <cristian_soncco@msn.com>
2025-01-06 23:42:43 +0100

    Implementar clases de dominio y mapeadores para la gestión de categorías y productos
    - Se creó la clase Category con atributos para id, nombre y estado.
    - Se implementó la clase Product con atributos para id, nombre, categoría, precio, stock y estado, incluyendo una referencia a Category.
    - Se añadió CategoryMapper para mapear entre la entidad Categoria y la clase Category.
    - Se introdujo ProductMapper para mapear entre la entidad Producto y la clase Product, utilizando CategoryMapper para la relación de categorías.

---
+++ src/main/java/com/crislearning/soncco_market/domain/Category.java
@@ -0,0 +1,31 @@
+package com.crislearning.soncco_market.domain;
+
+public class Category {
+    private int categoryId;
+    private String category;
+    private Integer active;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/crislearning/soncco_market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,35 @@
+package com.crislearning.soncco_market.persistence.mapper;
+
+
+
+import java.util.List;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import com.crislearning.soncco_market.domain.Product;
+import com.crislearning.soncco_market.persistence.entity.Producto;
+
+@Mapper(componentModel = "spring",uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+        @Mapping(source = "idProducto",target = "productoId"),
+        @Mapping(source = "nombre",target = "name"),
+        @Mapping(source = "idCategoria",target = "categoryId"),
+        @Mapping(source = "precioVenta",target = "price"),
+        @Mapping(source = "cantidadStock",target = "stock"),
+        @Mapping(source = "estado",target = "active"),
+        @Mapping(source = "categoria",target = "Category"),
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProducts(List<Producto> productos);
+
+    List<Producto> toProductos(List<Product> products);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras",ignore = true)
+    Producto toProducto(Product product);
+
+}
